cc3ce7d47a369a0c2564d14c5aa78fe95b073325
Cynthia Sturton <csturton@cs.unc.edu>
2024-11-20 12:14:57 -0500

    remove escape chars

--- hackatdac21/properties.tcl
+++ hackatdac21/properties.tcl
@@ -1,17 +1,17 @@
-assert -name HACK@DAC21\_p1 {(\~dmi\_jtag\_i.trst\_ni \|-> dmi\_jtag\_i.pass\_check == 1'b0)}
-assert -name HACK@DAC21\_p2 {(dmi\_jtag\_i.state\_q == dmi\_jtag\_i.Idle && dmi\_jtag\_i.state\_d == dmi\_jtag\_i.Write \|-> dmi\_jtag\_i.pass\_check == 1'b1)}
-assert -name HACK@DAC21\_p7 {(\~(ariane\_i.csr\_regfile\_i.debug\_mode\_q) \|\| (riscv::PRIV\_LVL\_M))}
-assert -name HACK@DAC21\_p14 {( (aes0\_wrapper\_i.aes.uut.validCounter - 1) == $next(aes0\_wrapper\_i.aes.uut.validCounter))}
-assert -name HACK@DAC21\_p18 {(ariane\_i.csr\_regfile\_i.csr\_we && ariane\_i.csr\_regfile\_i.csr\_addr.address == riscv::CSR\_SIE) -> ariane\_i.csr\_regfile\_i.mie\_d == (ariane\_i.csr\_regfile\_i.mie\_q & \~ariane\_i.csr\_regfile\_i.mideleg\_q) \| (ariane\_i.csr\_regfile\_i.csr\_wdata & ariane\_i.csr\_regfile\_i.mideleg\_q)}
-assert -name HACK@DAC21\_p30 {((dmi\_jtag\_i.pass\_mode) \|-> (dmi\_jtag\_i.pass\_data == dmi\_jtag\_i.data\_d))}
-assert -name HACK@DAC21\_p35 {\~(reglk\_wrapper\_i.rst\_ni && \~reglk\_wrapper\_i.jtag\_unlock && \~reglk\_wrapper\_i.rst\_9) \|-> (reglk\_wrapper\_i.reglk\_mem == 'h0)}
-assert -name HACK@DAC21\_p36 {((sha256\_wrapper\_i.sha256.sha256\_ctrl\_reg == sha256\_wrapper\_i.sha256.CTRL\_IGNORE && sha256\_wrapper\_i.sha256.ignore\_input\_reg) \|-> (sha256\_wrapper\_i.data == 0))}
-assert -name HACK@DAC21\_p39 {(aes0\_wrapper\_i.ct\_valid \|-> ((aes0\_wrapper\_i.p\_c[0] == 0) && (aes0\_wrapper\_i.p\_c[1] == 0) && (aes0\_wrapper\_i.p\_c[2] == 0) && (aes0\_wrapper\_i.p\_c[3] == 0)))}
-assert -name HACK@DAC21\_p42 {((acct\_wrapper\_i.rst\_ni && rst\_6) \|\| (acct\_wrapper\_i.acct\_mem.read\_data\_0[0]==32'h0000\_0000))}
-assert -name HACK@DAC21\_p46 {((aes0\_wrapper\_i.debug\_mode\_i) \|-> ((aes0\_wrapper\_i.key\_big == 192'b0) && (aes0\_wrapper\_i.key\_big2 == 192'b0)))}
-assert -name HACK@DAC21\_p47 {((aes0\_wrapper\_i.debug\_mode\_i) \|-> ((aes0\_wrapper\_i.key\_big == 192'b0) && (aes0\_wrapper\_i.key\_big2 == 192'b0)))}
-assert -name HACK@DAC21\_p48 {(\~(reglk\_wrapper\_i.rst\_ni && \~rst\_9) \|-> \~reglk\_wrapper\_i.jtag\_unlock)}
-assert -name HACK@DAC21\_p57 {(dma\_i.dma\_ctrl\_reg == dma\_i.CTRL\_ABORT && !dma\_i.done\_i \|=> dma\_i.dma\_ctrl\_reg != dma\_i.CTRL\_ABORT)}
-assert -name HACK@DAC21\_p84 {((dmi\_jtag\_i.dmi\_req\_ready && dmi\_jtag\_i.state\_q == dmi\_jtag\_i.Write) \|=> (dmi\_jtag\_i.state\_q == dmi\_jtag\_i.WaitWriteValid))}
-assert -name HACK@DAC21\_p95 {(\~(rsa\_wrapper\_i.rst\_ni && \~rsa\_wrapper\_i.rst\_13) \|-> (rsa\_wrapper\_i.msg\_out == 0))}
-assert -name HACK@DAC21\_p96\_modified {(riscv\_peripherals\_i.ariane\_boot\_sel\_i \|-> riscv\_peripherals\_i.rom\_rdata\_linux)}
+assert -name HACK@DAC21_p1 {(~dmi_jtag_i.trst_ni |-> dmi_jtag_i.pass_check == 1'b0)}
+assert -name HACK@DAC21_p2 {(dmi_jtag_i.state_q == dmi_jtag_i.Idle && dmi_jtag_i.state_d == dmi_jtag_i.Write |-> dmi_jtag_i.pass_check == 1'b1)}
+assert -name HACK@DAC21_p7 {(~(ariane_i.csr_regfile_i.debug_mode_q) || (riscv::PRIV_LVL_M))}
+assert -name HACK@DAC21_p14 {( (aes0_wrapper_i.aes.uut.validCounter - 1) == $next(aes0_wrapper_i.aes.uut.validCounter))}
+assert -name HACK@DAC21_p18 {(ariane_i.csr_regfile_i.csr_we && ariane_i.csr_regfile_i.csr_addr.address == riscv::CSR_SIE) -> ariane_i.csr_regfile_i.mie_d == (ariane_i.csr_regfile_i.mie_q & ~ariane_i.csr_regfile_i.mideleg_q) | (ariane_i.csr_regfile_i.csr_wdata & ariane_i.csr_regfile_i.mideleg_q)}
+assert -name HACK@DAC21_p30 {((dmi_jtag_i.pass_mode) |-> (dmi_jtag_i.pass_data == dmi_jtag_i.data_d))}
+assert -name HACK@DAC21_p35 {~(reglk_wrapper_i.rst_ni && ~reglk_wrapper_i.jtag_unlock && ~reglk_wrapper_i.rst_9) |-> (reglk_wrapper_i.reglk_mem == 'h0)}
+assert -name HACK@DAC21_p36 {((sha256_wrapper_i.sha256.sha256_ctrl_reg == sha256_wrapper_i.sha256.CTRL_IGNORE && sha256_wrapper_i.sha256.ignore_input_reg) |-> (sha256_wrapper_i.data == 0))}
+assert -name HACK@DAC21_p39 {(aes0_wrapper_i.ct_valid |-> ((aes0_wrapper_i.p_c[0] == 0) && (aes0_wrapper_i.p_c[1] == 0) && (aes0_wrapper_i.p_c[2] == 0) && (aes0_wrapper_i.p_c[3] == 0)))}
+assert -name HACK@DAC21_p42 {((acct_wrapper_i.rst_ni && rst_6) || (acct_wrapper_i.acct_mem.read_data_0[0]==32'h0000_0000))}
+assert -name HACK@DAC21_p46 {((aes0_wrapper_i.debug_mode_i) |-> ((aes0_wrapper_i.key_big == 192'b0) && (aes0_wrapper_i.key_big2 == 192'b0)))}
+assert -name HACK@DAC21_p47 {((aes0_wrapper_i.debug_mode_i) |-> ((aes0_wrapper_i.key_big == 192'b0) && (aes0_wrapper_i.key_big2 == 192'b0)))}
+assert -name HACK@DAC21_p48 {(~(reglk_wrapper_i.rst_ni && ~rst_9) |-> ~reglk_wrapper_i.jtag_unlock)}
+assert -name HACK@DAC21_p57 {(dma_i.dma_ctrl_reg == dma_i.CTRL_ABORT && !dma_i.done_i |=> dma_i.dma_ctrl_reg != dma_i.CTRL_ABORT)}
+assert -name HACK@DAC21_p84 {((dmi_jtag_i.dmi_req_ready && dmi_jtag_i.state_q == dmi_jtag_i.Write) |=> (dmi_jtag_i.state_q == dmi_jtag_i.WaitWriteValid))}
+assert -name HACK@DAC21_p95 {(~(rsa_wrapper_i.rst_ni && ~rsa_wrapper_i.rst_13) |-> (rsa_wrapper_i.msg_out == 0))}
+assert -name HACK@DAC21_p96_modified {(riscv_peripherals_i.ariane_boot_sel_i |-> riscv_peripherals_i.rom_rdata_linux)}
