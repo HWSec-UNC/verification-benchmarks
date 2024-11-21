c0a219c79fd3af322286cb83ea09d7892a26851d
Cynthia Sturton <csturton@cs.unc.edu>
2024-11-21 05:42:58 -0500

    update Hack@DAC 18 properties

--- hackatdac18/properties.tcl
+++ hackatdac18/properties.tcl
@@ -12,7 +12,7 @@ assert -name HACKDAC_p11 {(~( (riscv_core.debug_unit_i.dbg_halt != 1) && (riscv_
 assert -name HACKDAC_p12 {(~(adbg_tap_top.passchk ==1) && (adbg_tap_top.correct <= 31))}
 assert -name HACKDAC_p13 {(riscv_core.id_stage_i.controller_i.ctrl_fsm_ns == riscv_core.id_stage_i.controller_i.DECODE) |=> (riscv_core.id_stage_i.controller_i.ctrl_fsm_ns != riscv_core.id_stage_i.controller_i.DECODE)}
 assert -name HACKDAC_p14 {((( (riscv_core.ex_stage_i.alu_i.vector_mode_i == riscv_core.ex_stage_i.alu_i.VEC_MODE16) || (riscv_core.ex_stage_i.alu_i.vector_mode_i == riscv_core.ex_stage_i.alu_i.VEC_MODE8) ) |-> riscv_core.ex_stage_i.alu_i.adder_in_a[18] == 1'b0) )}
-
+assert -name HACKDAC_p15 {(rtc_clock.r_seconds < 8’h59)}
 assert -name HACKDAC_p16 {(adbg_tap_top.trstn_pad_i) || (adbg_tap_top.correct==0)}
 assert -name HACKDAC_p21 {(~(mux_func.c == mux_func.temperature_out))}
 assert -name HACKDAC_p27 {riscv_core.cs_registers_i.csr_we_int |-> riscv_core.cs_registers_i.PULP_SECURE}
